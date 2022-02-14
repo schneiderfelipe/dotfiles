b0419077c4ce3f07047c5bed83aa0f08923765e0
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2022-02-14 11:57:22 -0300

    Remember Chemcraft hasn't been set up for bash

--- .oh-my-zsh-custom/chemcraft.zsh
+++ .oh-my-zsh-custom/chemcraft.zsh
@@ -1,2 +1,3 @@
-# Use Chemcraft in a more ergonomic way
+# Use Chemcraft in a more ergonomic way.
+# This will only be available in zsh, I haven't set this up for bash.
 alias chemcraft="wine /opt/Chemcraft/Chemcraft.exe"
