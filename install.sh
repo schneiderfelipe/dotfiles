7c05732709df73a29538abf570fc229cc6d9c18a
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2022-02-20 16:47:06 -0300

    Attempt to install things requiring super user privileges

--- install.sh
+++ install.sh
@@ -24,6 +24,7 @@ function code() {
     echo -n "$(tput sgr0)"
 }
 
+
 info "This will install most of what is needed for my dotfiles... "
 warning "proceed? [y/n]?"
 old_stty_cfg=$(stty -g)
@@ -84,33 +85,46 @@ fi
 echo
 sleep 0.5
 
+
 warning "Things that need to be installed manually:\n\n"
-
-info "zsh:\n"
-code "sudo apt install zsh\n\n"
-
-info "tmux:\n"
-code "sudo apt install tmux\n\n"
-
-info "neovim:\n"
-code "sudo add-apt-repository ppa:neovim-ppa/stable\n"
-code "sudo apt update\n"
-code "sudo apt install neovim\n\n"
 
 info "bat:\n"
 code "https://github.com/sharkdp/bat/releases\n\n"
 
-info "fzf:\n"
-code "sudo apt install fzf\n\n"
-
-info "ripgrep:\n"
-code "sudo apt install ripgrep\n\n"
-
 info "fd:\n"
 code "https://github.com/sharkdp/fd/releases\n\n"
+
+info "alacritty:\n"
+code "https://github.com/alacritty/alacritty/blob/master/INSTALL.md#manual-installation\n\n"
 
 info "JetBrains Mono:\n"
 code "https://github.com/ryanoasis/nerd-fonts/releases\n\n"
 
-info "alacritty:\n"
-code "https://github.com/alacritty/alacritty/blob/master/INSTALL.md#manual-installation\n\n"
+
+info "Some things require super user privileges... "
+warning "proceed? [y/n]?"
+old_stty_cfg=$(stty -g)
+stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_cfg
+if ! echo "$answer" | grep -iq "^y"; then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+echo
+echo
+
+info "Attempting to install zsh...\n"
+sudo apt install zsh -y
+
+info "Attempting to install tmux...\n"
+sudo apt install tmux -y
+
+info "Attempting to install fzf...\n"
+sudo apt install fzf -y
+
+info "Attempting to install ripgrep...\n"
+sudo apt install ripgrep -y
+
+info "Attempting to install neovim...\n"
+sudo add-apt-repository ppa:neovim-ppa/stable -y
+sudo apt update
+sudo apt install neovim -y
