839f7d02f1acc6556ca570a5f40cba7aa3e3b1aa
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2022-11-19 10:51:03 -0300

    Update installation of gh

--- install.sh
+++ install.sh
@@ -241,10 +241,11 @@ pip install -U pynvim
 
 if ! command -v "gh" > /dev/null 2>&1; then
     info "Installing github/cli...\n"
-    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-
-    sudo apt install gh -y
+    type -p curl >/dev/null || sudo apt install curl -y
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+        && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+        && sudo apt install gh -y
 else
     success "github/cli is already installed\n"
 fi
