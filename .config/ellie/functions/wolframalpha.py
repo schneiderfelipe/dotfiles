0fa01913cbdd7a650777a4b17a51f6b8f9669117
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-09-24 19:29:15 -0300

    refactor(.config/ellie): rename function

--- .config/ellie/functions/wolframalpha.py
+++ .config/ellie/functions/wolframalpha.py
@@ -7,7 +7,7 @@ import requests
 appid = os.environ["WOLFRAM_ALPHA_APPID"]
 
 
-def wolframalpha(input, appid=appid):
+def wolfram_alpha(input, appid=appid):
     endpoint = "https://www.wolframalpha.com/api/v1/llm-api"
     headers = {"Authorization": f"Bearer {appid}"}
     # TODO: there are other parameters available, check them out: https://products.wolframalpha.com/llm-api/documentation
@@ -18,14 +18,14 @@ def wolframalpha(input, appid=appid):
 match sys.argv[1:]:  # ignore script name
     case []:
         kwargs = {"appid": appid, **json.load(sys.stdin)}
-        output = wolframalpha(**kwargs)
+        output = wolfram_alpha(**kwargs)
         print(output)
     case ["spec"]:
         print(
             json.dumps(
                 {
-                    "name": "wolframalpha",
-                    "description": "Understand natural language queries about various subjects, perform mathematical calculations and conversions, and obtain up-to-date data",  # noqa: E501
+                    "name": "compute",
+                    "description": "Compute expert-level answers using WolframAlpha",
                     "parameters": {
                         "type": "object",
                         "required": ["input"],
